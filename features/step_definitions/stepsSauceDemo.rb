86dd7516de7aa3048c3ade9148cbb498ed704615
Rajat Gupta <rajat.gupta@veeam.com>
2023-11-17 14:27:58 +0530

    Fix Layout

--- features/step_definitions/stepsSauceDemo.rb
+++ features/step_definitions/stepsSauceDemo.rb
@@ -5,24 +5,24 @@ require 'rspec'
 driver = Selenium::WebDriver.for :chrome
 driver.manage.window.maximize
 
-Given("open SauceDemo home page") do
-    driver.navigate.to "https://saucedemo.com/"
-    sleep(3)
+Given('open SauceDemo home page') do
+  driver.navigate.to 'https://saucedemo.com/'
+  sleep(3)
 end
 
-Then("enter username and password") do
-    driver.find_element(:id, "user-name").send_keys("standard_user")
-    sleep(3)
-    driver.find_element(:id, "password").send_keys("secret_sauce")
-    sleep(3)
+Then('enter username and password') do
+  driver.find_element(:id, 'user-name').send_keys('standard_user')
+  sleep(3)
+  driver.find_element(:id, 'password').send_keys('secret_sauce')
+  sleep(3)
 end
 
-Then("click on login button") do
-    driver.find_element(:id, "login-button").click()
-    sleep(3)
+Then('click on login button') do
+  driver.find_element(:id, 'login-button').click()
+  sleep(3)
 end
 
-Then("verify SauceDemo landing page") do
-    expect(driver.find_element(:class, "title").text).to eq("Products")
-    sleep(3)
+Then('verify SauceDemo landing page') do
+  expect(driver.find_element(:class, 'title').text).to eq('Products')
+  sleep(3)
 end
